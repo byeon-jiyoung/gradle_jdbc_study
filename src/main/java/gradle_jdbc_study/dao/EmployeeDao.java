a6c9c332002bb6e45bdb32d4e8fcdd024473de2d
변지영 <bjy8668@hanmail.net>
2019-06-28 17:32:06 +0900

    DAO interface추가(EmployeeDao, DepartmentDao)

---
+++ src/main/java/gradle_jdbc_study/dao/EmployeeDao.java
@@ -0,0 +1,17 @@
+package gradle_jdbc_study.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import gradle_jdbc_study.dto.Employee;
+
+public interface EmployeeDao {
+	List<Employee> selectEmployeeByAll() throws SQLException;
+	Employee selectEmployeeByNo(Employee employee) throws SQLException;
+	int insertEmployee(Employee employee) throws SQLException ;
+	int deleteEmployee(Employee employee) throws SQLException;
+	int updateEmployee(Employee employee) throws SQLException;
+}
+
+//ctrl + shift + o 자동 imployee
+//ctrl + shitf + f 자동정렬
